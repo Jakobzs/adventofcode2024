5304dc2ae3f51405da0bdc49bd7dba56e0ac5942
Jakob <31919330+Jakobzs@users.noreply.github.com>
2024-12-01 18:53:22 +0100

    solve part two

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,4 +1,5 @@
 use adventofcode2024::AocSolution;
+use std::collections::HashMap;
 
 pub struct Solution;
 
@@ -40,53 +41,35 @@ impl AocSolution for Solution {
     }
 
     fn part2(&self, input: &str) -> u64 {
-        input
-            .lines()
-            .map(|line| {
-                let values = parse_line(line);
+        let mut left_list = Vec::new();
+        let mut right_list = Vec::new();
 
-                let first = values[0];
-                let last = values.last().unwrap();
+        for line in input.lines() {
+            let values = line.split("   ").collect::<Vec<&str>>();
 
-                first * 10 + last
-            })
-            .sum::<u32>() as u64
+            let first = values[0].parse::<i32>().unwrap();
+            let last = values[1].parse::<i32>().unwrap();
+
+            left_list.push(first);
+            right_list.push(last);
+        }
+
+        // Create a frequency map for the right list
+        let mut right_frequency = HashMap::new();
+        for num in &right_list {
+            *right_frequency.entry(num).or_insert(0) += 1;
+        }
+
+        // Calculate the similarity score
+        let mut similarity_score = 0;
+        for num in &left_list {
+            if let Some(&count) = right_frequency.get(&num) {
+                similarity_score += num * count;
+            }
+        }
+
+        similarity_score as u64
     }
-}
-
-fn parse_line(line: &str) -> Vec<u32> {
-    let line = line.to_ascii_lowercase();
-    (0..line.len())
-        .map(|i| &line[i..])
-        .filter_map(|wnd| {
-            let c = wnd.chars().next().unwrap();
-            if c.is_ascii_digit() {
-                return c.to_digit(10);
-            }
-
-            if wnd.starts_with("one") {
-                Some(1)
-            } else if wnd.starts_with("two") {
-                Some(2)
-            } else if wnd.starts_with("three") {
-                Some(3)
-            } else if wnd.starts_with("four") {
-                Some(4)
-            } else if wnd.starts_with("five") {
-                Some(5)
-            } else if wnd.starts_with("six") {
-                Some(6)
-            } else if wnd.starts_with("seven") {
-                Some(7)
-            } else if wnd.starts_with("eight") {
-                Some(8)
-            } else if wnd.starts_with("nine") {
-                Some(9)
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<_>>()
 }
 
 adventofcode2024::run!(Solution);

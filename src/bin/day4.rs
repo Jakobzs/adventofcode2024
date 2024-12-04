501149f5f1ad2d7a6eabf78a042208df7417b163
Jakob <31919330+Jakobzs@users.noreply.github.com>
2024-12-04 09:16:35 +0100

    prep day 4

---
+++ src/bin/day4.rs
@@ -0,0 +1,22 @@
+use adventofcode2024::AocSolution;
+use regex::Regex;
+
+pub struct Solution;
+
+impl AocSolution for Solution {
+    const DAY: u8 = 3;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn part1<T: AsRef<str>>(&self, input: T) -> u64 {
+        0
+    }
+
+    fn part2<T: AsRef<str>>(&self, input: T) -> u64 {
+        0
+    }
+}
+
+adventofcode2024::run!(Solution);
